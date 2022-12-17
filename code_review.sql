68c8484d1c805f5365f6e8ab37f191f1559a07d8
Reed-Carter <reedandrew18@gmail.com>
2022-12-17 11:35:28 -0800

    complete manipulating values q1

--- code_review.sql
+++ code_review.sql
@@ -6,3 +6,6 @@ SELECT p.id, p.name FROM part_categories AS p WHERE name LIKE '%Bricks%';
 --Sub queries
 SELECT name FROM sets WHERE theme_id IN (SELECT id FROM themes WHERE name LIKE '%Star Wars%' OR name LIKE '%Pirates%');
 SELECT * FROM inventory_parts WHERE inventory_id IN (SELECT id FROM inventories WHERE version > 1);
+
+--Manipulate Values in Select
+SELECT s.year, UPPER(CONCAT(s.name, '!!!')) FROM sets AS s WHERE name LIKE '%Batman%';
