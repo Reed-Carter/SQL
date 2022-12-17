0319a4da8ed3095f371e29611ef132099d56bd18
Reed-Carter <reedandrew18@gmail.com>
2022-12-17 10:51:00 -0800

    complete sub-queries

--- code_review.sql
+++ code_review.sql
@@ -1,4 +1,8 @@
 --Selecting Values
 SELECT name, part_num FROM parts WHERE name LIKE '%Hair%' ORDER BY part_num DESC;
-SELECT * from sets WHERE year BETWEEN 1990 and 2015 and name like '%Showdown%';
+SELECT * FROM sets WHERE year BETWEEN 1990 AND 2015 AND name LIKE '%Showdown%';
 SELECT p.id, p.name FROM part_categories AS p WHERE name LIKE '%Bricks%'; 
+
+--Sub queries
+SELECT name FROM sets WHERE theme_id IN (SELECT id FROM themes WHERE name LIKE '%Star Wars%' OR name LIKE '%Pirates%');
+SELECT * FROM inventory_parts WHERE inventory_id IN (SELECT id FROM inventories WHERE version > 1);
